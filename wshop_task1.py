73b4bac1b5fc7183f58079e70cc1276d92628811
HariniRR <141791926+HariniRR@users.noreply.github.com>
2024-08-10 10:32:22 +0530

    Add files via upload

---
+++ wshop_task1.py
@@ -0,0 +1,11 @@
+#a=list(map(int(input("enter an array").split())))
+a=list()
+v=(int(input("enter an array").split()))
+a.append(v)
+print(a)
+b=int(input("enter a number"))
+c=0
+for i in a:
+    if a[i]<b:
+        c+=1
+print (c)
