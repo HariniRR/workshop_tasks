fba653e9111fc55a52747ec3f8c9788eeaf43640
HariniRR <141791926+HariniRR@users.noreply.github.com>
2024-08-10 20:52:19 +0530

    Update wshop_task1.py

--- wshop_task1.py
+++ wshop_task1.py
@@ -1,9 +1,8 @@
 #num of elements in an array that are < a specific num 
 #a=list(map(int(input("enter an array").split())))
-a=list()
-v=(int(input("enter an array").split()))
+a = []
+v = list(map(int, input("Enter an array: ").split()))
 a.append(v)
-print(a)
 b=int(input("enter a number"))
 c=0
 for i in a:
